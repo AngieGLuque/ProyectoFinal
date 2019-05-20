1bc05880f06e783ed96019db8b3460419efed00a
IlI <mariovaac@unisabana.edu.co>
2019-05-20 11:06:54 -0500

    qwe

--- app/src/main/java/pnt/co/edu/unisabana/MainActivity.java
+++ app/src/main/java/pnt/co/edu/unisabana/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import retrofit2.Retrofit;
+//import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,9 +29,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(open);
             }
         });
-        Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("https://infinite-beyond-12720.herokuapp.com/")
-            .build();
-        final HerokuService service = retrofit.create(HerokuService.class);
+        //Retrofit retrofit = new Retrofit.Builder()
+        //    .baseUrl("https://infinite-beyond-12720.herokuapp.com/")
+        //    .build();
+        //final HerokuService service = retrofit.create(HerokuService.class);
     }
 }
