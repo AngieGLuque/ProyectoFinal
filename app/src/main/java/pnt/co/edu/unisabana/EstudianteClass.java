d7e74ec6ed906d53b5b90074920f2f92d43bff76
IlI <mariovaac@unisabana.edu.co>
2019-05-20 09:04:46 -0500

    qwe

--- app/src/main/java/pnt/co/edu/unisabana/EstudianteClass.java
+++ app/src/main/java/pnt/co/edu/unisabana/EstudianteClass.java
@@ -14,7 +14,7 @@ public class EstudianteClass {
         this.email = email;
         this.contrasena = contrasena;
     }
-    //Hola
+    //Hola como estas
     public String getNombre() {
         return nombre;
     }
