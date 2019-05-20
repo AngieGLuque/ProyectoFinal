c2675cae8b2a3c47179fa023c66edd8a70513920
AngieGLuque <angie.luque08@gmail.com>
2019-05-20 09:13:19 -0500

    miu

--- app/src/main/java/pnt/co/edu/unisabana/Singleton.java
+++ app/src/main/java/pnt/co/edu/unisabana/Singleton.java
@@ -2,13 +2,20 @@ package pnt.co.edu.unisabana;
 
 public class Singleton {
     private static EstudianteClass estudiante;
-    public static  BibliotecarioClass bibliotecario;
+    private static  BibliotecarioClass bibliotecario;
 
     public static  EstudianteClass getInstanceEstudiante(){
         if(estudiante == null){
             Singleton.estudiante = EstudianteClass.getInstanceOf();
         }
         return estudiante;
+    }
+
+    public  static  BibliotecarioClass getInstanceBibliotecario(){
+        if(bibliotecario == null){
+            Singleton.bibliotecario = BibliotecarioClass.getInstanceOf();
+        }
+        return  bibliotecario;
     }
 
 }
